0c351e3a897bf6de0deea1d920fac03398c48938
Linda Johnstone <linda.johnstone@myob.com>
2021-08-18 08:52:15 +1000

    Refactoring MapParser.ParseMap + Validator.IsValidMap
    All tests passing

--- MarsRover/Validator.cs
+++ MarsRover/Validator.cs
@@ -6,37 +6,28 @@ namespace MarsRover
 {
     public static class Validator
     {
-        public static bool IsValidMap(string input)
+        public static bool IsValidMap(string input) // TODO: refactor
         {
             var mapElements = SplitInput(input, "\n");
             var numberOfElements = mapElements.Length;
             if (numberOfElements == 1) return false;
-            var elementList = new List<int>();
+            for (var i = 0; i < numberOfElements - 1; i++)
+            {
+                if (mapElements[i].Length != mapElements[i + 1].Length) return false;
+            }
             var mapCharsCount = 0;
             var count = 0;
-            CountValidChars(mapElements, elementList, ref mapCharsCount, ref count);
-            if (count == mapCharsCount) return true;
-            if (elementList.Distinct().Count() != 1) return false;
-            return true;
-        }
-
-        private static void CountValidChars(string[] mapElements, List<int> elementList, ref int mapCharsCount, ref int count)
-        {
             foreach (var element in mapElements)
             {
                 var mapChars = element.ToCharArray();
                 mapCharsCount = mapChars.Length;
-                elementList.Add(mapCharsCount);
-                CountValidChars(count, mapChars);
+                foreach (var mapChar in mapChars)
+                {
+                    if (mapChar == 'O' || mapChar == 'N') count++;
+                }
             }
-        }
-
-        private static void CountValidChars(int count, char[] mapChars)
-        {
-            foreach (var mapChar in mapChars)
-            {
-                if (mapChar == 'O' || mapChar == 'N') count++;
-            }
+            if (count == mapCharsCount) return true;
+            return true;
         }
 
         private static string[] SplitInput(string input, string delimiter)

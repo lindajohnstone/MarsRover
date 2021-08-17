18175884607c4d0ca4b798a769fbfcce09b3c923
Linda Johnstone <linda.johnstone@myob.com>
2021-08-17 15:25:59 +1000

    Test InlineData changed to using params
    All tests passing

--- MarsRover.Tests/FileInputShould.cs
+++ MarsRover.Tests/FileInputShould.cs
@@ -6,12 +6,13 @@ namespace MarsRover.Tests
     public class FileInputShould
     {
         [Theory]
-        [InlineData("ONNN\nNNNN\nNNNN", "TestFiles/testFile1.txt")]
-        [InlineData("ONNNNNNNNNNN\nONNNNNNONNNN\nONNNNNNNNNNN\nONNNNNNNNNNN\nONNNNNNNNNNN\nONNNNNNNNNNN\nONNNNNNNNNNN\nONNNNNNNNNNN", "TestFiles/testFile2.txt")]
-        [InlineData("ONNNNNNN\nNNNNONNN\nNNNNNNNN", "TestFiles/testFile3.txt")]
-        [InlineData("ONNNNNNN\nNNNNONNN\nNNNNONNO\nNNNNONNN\nNNNNNNNN", "TestFiles/testFile4.txt")]
-        public void ReturnString_GivenFileInput(string expected, string path)
+        [InlineData("TestFiles/testFile1.txt", "ONNN", "NNNN", "NNNN")]
+        [InlineData("TestFiles/testFile2.txt", "ONNNNNNNNNNN", "ONNNNNNONNNN", "ONNNNNNNNNNN", "ONNNNNNNNNNN", "ONNNNNNNNNNN", "ONNNNNNNNNNN", "ONNNNNNNNNNN", "ONNNNNNNNNNN")]
+        [InlineData("TestFiles/testFile3.txt", "ONNNNNNN", "NNNNONNN", "NNNNNNNN")]
+        [InlineData("TestFiles/testFile4.txt", "ONNNNNNN", "NNNNONNN", "NNNNONNO", "NNNNONNN", "NNNNNNNN")]
+        public void ReturnString_GivenFileInput(string path, params string[] inputs)
         {
+            var expected = String.Join(Environment.NewLine, inputs);
             var input = new FileInput();
 
             var result = input.Read(path);

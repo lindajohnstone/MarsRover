13a981ced98f1d7bd88ec997fb95d30f3b0cb84b
Linda Johnstone <linda.johnstone@myob.com>
2021-08-19 15:57:20 +1000

    InputParser.IsValidLocation
    All tests passing

--- MarsRover.Tests/InputParserShould.cs
+++ MarsRover.Tests/InputParserShould.cs
@@ -1,4 +1,5 @@
 using System;
+using FluentAssertions;
 using Xunit;
 
 namespace MarsRover.Tests
@@ -14,11 +15,24 @@ namespace MarsRover.Tests
         [InlineData("e", Direction.East)]
         [InlineData("W", Direction.West)]
         [InlineData("w", Direction.West)]
-        public void ReturnRoverStartDirection_GivenValidString(string input, Direction expected)
+        public void ReturnRoverInitialDirection_GivenValidString(string input, Direction expected)
         {
             var result = InputParser.ParseDirection(input);
 
             Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData("0,0", 0, 0)]
+        [InlineData("100,8", 100, 8)]
+        [InlineData("1,80", 1, 80)]
+        public void ReturnRoverInitialLocation_GivenValidString(string input, int x, int y)
+        {
+            var expected = new Location(x, y);
+
+            var result = InputParser.ParseLocation(input);
+
+            result.Should().BeEquivalentTo(expected);
         }
     }
 }
--- MarsRover/InputParser.cs
+++ MarsRover/InputParser.cs
@@ -13,5 +13,18 @@ namespace MarsRover
             if (input.Equals("W", StringComparison.InvariantCultureIgnoreCase)) return Direction.West;
             return Direction.None;
         }
+
+        public static Location ParseLocation(string input)
+        {
+            var coordinates = SplitInput(input, ",");
+            var x = Int32.Parse(coordinates[0]);
+            var y = Int32.Parse(coordinates[1]);
+            return new Location(x, y);
+        }
+
+        private static string[] SplitInput(string input, string delimiter)
+        {
+            return input.Split(delimiter, StringSplitOptions.RemoveEmptyEntries);
+        }
     }
 }

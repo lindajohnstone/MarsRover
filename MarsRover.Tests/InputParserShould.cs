a9c89710c232685de58c4d4b573cd51b996f95fb
Linda Johnstone <linda.johnstone@myob.com>
2021-08-19 11:46:56 +1000

    Refactor InputParser.ParseDirection
    All tests passing

--- MarsRover.Tests/InputParserShould.cs
+++ MarsRover.Tests/InputParserShould.cs
@@ -9,8 +9,11 @@ namespace MarsRover.Tests
         [InlineData("N", Direction.North)]
         [InlineData("n", Direction.North)]
         [InlineData("S", Direction.South)]
+        [InlineData("s", Direction.South)]
         [InlineData("E", Direction.East)]
+        [InlineData("e", Direction.East)]
         [InlineData("W", Direction.West)]
+        [InlineData("w", Direction.West)]
         public void ReturnRoverStartDirection_GivenValidString(string input, Direction expected)
         {
             var result = InputParser.ParseDirection(input);
--- MarsRover/InputParser.cs
+++ MarsRover/InputParser.cs
@@ -7,20 +7,11 @@ namespace MarsRover
     {
         public static Direction ParseDirection(string input)
         {
-            var upperInput = input.ToUpper();
-            return upperInput switch {
-                "N" => Direction.North,
-                "S" => Direction.South,
-                "E" => Direction.East,
-                "W" => Direction.West,
-                _ => Direction.None
-            };
-
-            // if (input.Equals("N", StringComparison.InvariantCultureIgnoreCase)) return Direction.North;
-            // if (input == "S" || input == "s") return Direction.South;
-            // if (input == "E") return Direction.East;
-            // if (input == "W") return Direction.West;
-            // return Direction.None;
+            if (input.Equals("N", StringComparison.InvariantCultureIgnoreCase)) return Direction.North;
+            if (input.Equals("S", StringComparison.InvariantCultureIgnoreCase)) return Direction.South;
+            if (input.Equals("E", StringComparison.InvariantCultureIgnoreCase)) return Direction.East;
+            if (input.Equals("W", StringComparison.InvariantCultureIgnoreCase)) return Direction.West;
+            return Direction.None;
         }
     }
 }

95ed8ac817683a06e4c24ad0c63d485447707a5a
Linda Johnstone <linda.johnstone@myob.com>
2021-08-23 16:58:14 +1000

    Refactor Rover.Turn - is it easier to read or have I gone too far?
    All tests passing

--- MarsRover/Rover.cs
+++ MarsRover/Rover.cs
@@ -18,15 +18,28 @@ namespace MarsRover
         public Location Location { get; private set; }
 
         public Direction Turn(Command command) // TODO: talk at Mentor meeting about changing to switch expression
-        {   
-            if (command == Command.TurnLeft && Direction == Direction.North) return Direction.West;
-            if (command == Command.TurnLeft && Direction == Direction.South) return Direction.East;
-            if (command == Command.TurnLeft && Direction == Direction.West) return Direction.South;
-            if (command == Command.TurnLeft && Direction == Direction.East) return Direction.North;
-            if (command == Command.TurnRight && Direction == Direction.North) return Direction.East;
-            if (command == Command.TurnRight && Direction == Direction.South) return Direction.West;
-            if (command == Command.TurnRight && Direction == Direction.West) return Direction.North;
-            if (command == Command.TurnRight && Direction == Direction.East) return Direction.South;
+        {
+            var turnLeft = command == Command.TurnLeft;
+            var turnRight = command == Command.TurnRight;
+            
+            var north = Direction == Direction.North;
+            var south = Direction == Direction.South;
+
+            var turnWest = Direction.West;
+            if ((turnLeft && north) || (turnRight && south)) return turnWest;
+
+            var turnEast = Direction.East;
+            if ((turnRight && north) || (turnLeft && south)) return turnEast;
+
+            var west = Direction == Direction.West;
+            var east = Direction == Direction.East;
+
+            var turnSouth = Direction.South;
+            if ((turnLeft && west) || (turnRight && east)) return turnSouth;
+
+            var turnNorth = Direction.North;
+            if ((turnRight && west) || (turnLeft && east)) return turnNorth;
+
             return Direction.None;
         }
 

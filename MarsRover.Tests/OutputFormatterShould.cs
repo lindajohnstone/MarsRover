20b442e3bedc48ac8f1ffcebea8f5258cb33760b
Linda Johnstone <linda.johnstone@myob.com>
2021-08-25 15:44:42 +1000

    OutputFormatter.FormatMap with Rover
    All tests passing

--- MarsRover.Tests/OutputFormatterShould.cs
+++ MarsRover.Tests/OutputFormatterShould.cs
@@ -9,12 +9,45 @@ namespace MarsRover.Tests
     {
         [Theory]
         [MemberData(nameof(OutputFormatterShouldTestData.FormatMapNoRoverTestData), MemberType = typeof(OutputFormatterShouldTestData))]
-        public void FormatMap_ReturnsMapWithNoRover_GivenValidString(List<Square> squares, int width, int height, params string[] inputs)
+        public void FormatMap_ReturnsMapWithNoRover_GivenMap(List<Square> squares, int width, int height, params string[] inputs)
         {
             var map = new Map(width, height, squares);
             var expectedMap = String.Join(Environment.NewLine, inputs);
 
             var result = OutputFormatter.FormatMap(map);
+
+            result.Should().BeEquivalentTo(expectedMap);
+        }
+
+        [Fact]
+        public void FormatMap_ReturnsMapWithRover_GivenMapAndRover()
+        {
+            var squares = new List<Square>
+            {
+                new Square(SquareContent.Obstacle, 0, 0),
+                new Square(SquareContent.None, 1, 0),
+                new Square(SquareContent.None, 2, 0),
+                new Square(SquareContent.None, 3, 0),
+                new Square(SquareContent.None, 0, 1),
+                new Square(SquareContent.None, 1, 1),
+                new Square(SquareContent.None, 2, 1),
+                new Square(SquareContent.None, 3, 1),
+                new Square(SquareContent.None, 0, 2),
+                new Square(SquareContent.None, 1, 2),
+                new Square(SquareContent.None, 2, 2),
+                new Square(SquareContent.None, 3, 2)
+            };
+            var map = new Map(4, 3, squares);
+            var inputs = new string[]
+            {
+                "ONNN",
+                "NNNN",
+                "NNRN"
+            };
+            var rover = new Rover(Direction.North, 2, 2);
+            var expectedMap = String.Join(Environment.NewLine, inputs);
+
+            var result = OutputFormatter.FormatMap(map, rover);
 
             result.Should().BeEquivalentTo(expectedMap);
         }
--- MarsRover/OutputFormatter.cs
+++ MarsRover/OutputFormatter.cs
@@ -24,5 +24,38 @@ namespace MarsRover
             }
             return stringBuilder.ToString().TrimEnd();
         }
+
+        public static string FormatMap(Map map, Rover rover)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            for (var y = 0; y < map.Height; y++)
+            {
+                for (var x = 0; x < map.Width; x++)
+                {
+                    var location = new Location(x, y);
+                    var square = map.GetSquareAtLocation(location);
+                    if (square.HasObstacle())
+                    {
+                        stringBuilder.Append("O");
+                    }
+                    if (!square.HasObstacle() && LocationsAreEqual(square.Location, rover.Location))
+                    {
+                        stringBuilder.Append("R");
+                    }
+                    if (!square.HasObstacle() && !LocationsAreEqual(square.Location, rover.Location)) 
+                    {
+                        stringBuilder.Append("N");
+                    }
+                }
+                stringBuilder.Append(Environment.NewLine);
+            }
+            return stringBuilder.ToString().TrimEnd();
+        }
+
+        public static bool LocationsAreEqual(Location location1, Location location2)
+        {
+            if (location1.X == location2.X && location1.Y == location2.Y) return true;
+            return false;
+        }
     }
 }
